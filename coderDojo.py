0c39970b854236f367ae93db292abadd0887cd1c
Roel Thijs <roel.thijs@gmail.com>
2018-12-16 20:23:12 +0100

    opkuis

--- coderDojo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from DeVestenBot import DeVestenBot
-
-bot = DeVestenBot()
-
-bot.draai_graden(60)
--- test_sound.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from ev3dev.ev3 import Sound
-
-Sound.speak("Coder dojo").wait()
-
-Sound.play('jajaja.wav').wait()
